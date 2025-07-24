5d8147d08043546961e142a63dc004ac6135a4a0
BoMin <bmlee@postech.ac.kr>
2025-07-24 16:16:38 +0900

    fix: String Extension 수정

--- Chaap/Chaap/Common/Extension/String+Extension.swift
+++ Chaap/Chaap/Common/Extension/String+Extension.swift
@@ -8,12 +8,10 @@
 import Foundation
 
 extension String {
-    /// 문자열의 UTF-8 바이트 수
     var utf8ByteCount: Int {
         return self.data(using: .utf8)?.count ?? 0
     }
-    
-    /// 주어진 바이트 수 이하로 잘린 문자열 반환
+
     func trimmedToMaxByteLength(_ maxBytes: Int) -> String {
         var trimmed = self
         while trimmed.utf8ByteCount > maxBytes {
