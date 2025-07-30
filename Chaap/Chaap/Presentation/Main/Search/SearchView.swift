7e73d3fc96a7f44ff101f01883262f2b60fff3e2
BoMin <bmlee@postech.ac.kr>
2025-07-30 16:26:53 +0900

    fix: onTapGesture 삭제

--- Chaap/Chaap/Presentation/Main/Search/SearchView.swift
+++ Chaap/Chaap/Presentation/Main/Search/SearchView.swift
@@ -24,10 +24,6 @@ struct SearchView: View {
             .safeAreaPadding(.horizontal, 16)
             .safeAreaPadding(.top, 9)
         }
-        //화면 터치시 키보드 숨김
-        .onTapGesture {
-            hideKeyboard()
-        }
         .navigationBarBackButtonHidden()
     }
     
@@ -105,12 +101,3 @@ struct SearchView: View {
         }
     }
 }
-
-//화면 터치시 키보드 숨김
-#if canImport(UIKit)
-extension View {
-    func hideKeyboard() {
-        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
-    }
-}
-#endif
